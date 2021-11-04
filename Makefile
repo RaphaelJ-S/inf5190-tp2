0a92298fa739fc26fe8d2ff16b715a97947a1d3c
Raphael Jacob-Simard <RaphaelJ-S@hotmail.com>
2021-11-04 08:10:17 -0400

    squelette de base pour le tp2

---
+++ Makefile
@@ -0,0 +1,7 @@
+
+
+run: app/src/app.py
+	python -m app.src.app
+
+test: app/tests/test.py
+	python -m app.tests.test
---
+++ app/tests/test.py
@@ -0,0 +1,9 @@
+from app.src.app import main
+
+
+def test():
+    print(main())
+
+
+if __name__ == "__main__":
+    test()
